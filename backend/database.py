2a21ba9acf95ac31f6ce03dcb8a2895fbcae217e
ali.mohamaad.turokmani <ali.mohamaad.turokmani@gmail.com>
2026-07-31 15:31:34 +0300

    Switch to pg8000

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,9 @@ SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./plants.db"
 
 # SQLAlchemy requires 'postgresql://' instead of 'postgres://'
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql+pg8000://", 1)
+elif SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgresql://", "postgresql+pg8000://", 1)
 
 # Connect arguments: check_same_thread is only needed for SQLite
 connect_args = {}
